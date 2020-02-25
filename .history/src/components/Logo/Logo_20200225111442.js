855d8879a9d00420761b7bd6d6573c74448342fd
Erik Baer <erik.baer@yahoo.de>
2020-02-25 11:20:38 +0100

    import and insert Logo

--- .history/src/components/Logo/Logo_20200225111442.js
+++ .history/src/components/Logo/Logo_20200225111442.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Tilt from 'react-tilt';
-import brain from './brain.png';
-import './Logo.css';
-
+import './Logo.css'
 
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2" options={{ max : 60 }} style={{ height: 250, width: 250 }} >
-                <div className="Tilt-inner"> <img src={brain} alt='brain'/> </div>
+                <div className="Tilt-inner"> 👽 </div>
             </Tilt>
         </div>
     );
--- .history/src/components/Logo/Logo_20200225111858.js
+++ .history/src/components/Logo/Logo_20200225111858.js
@@ -8,7 +8,7 @@ const Logo = () => {
     return (
         <div className='ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2" options={{ max : 60 }} style={{ height: 250, width: 250 }} >
-                <div className="Tilt-inner"> <img src={brain} alt='brain'/> </div>
+                <div className="Tilt-inner"> <img src=brain alt='brain>' </div>
             </Tilt>
         </div>
     );
