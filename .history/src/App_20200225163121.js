94287f29f4224fda365fcb6841594c09559b5a9d
Erik Baer <erik.baer@yahoo.de>
2020-02-25 16:31:47 +0100

    connect face-detect

---
+++ .history/src/App_20200225163121.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+import particlesOptions from './particlesjs-config';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: 'f7738781c1e0443593a11eec5113e60'
+ });
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input:'',
+    }
+  }
+
+onInputChange = (event) => console.log(event.target.value)
+
+onButtonSubmit = () => {
+  app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+     console.log(response)
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+  console.log('click')
+} ;
+
+  render(){
+    return (
+      <div className="App">
+        <Particles className="particles"
+        params={particlesOptions}
+        />
+        <Navigation/>  
+        <Logo/>
+        <Rank/>
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        {/*<FaceRecognition/>} */}
+      </div>
+    );
+  }
+  
+}
+
+export default App;
